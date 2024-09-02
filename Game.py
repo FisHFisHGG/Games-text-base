cef12374a4f10e1cc32c14b5508f227ab7ae98f0
FisHFisHGG <123833588+FisHFisHGG@users.noreply.github.com>
2024-09-02 15:23:23 +0300

    Game.py

--- Game.py
+++ Game.py
@@ -45,12 +45,45 @@ elif player_class == "R":
     print(f"You have {health} health")
 
 
-attack = 0
-
-If player_class == "W"
-    input("Select your weapon:\n"
+attack = 30
+# Weapon for classes
+if player_class == "W":
+   player_weapon = input("Select your weapon:\n"
 "A-A melee weapon and shield\n"
-"T-Two-handed weapon\n") 
+"T-Two-handed weapon\n")
+   if player_weapon == "A":
+       attack = attack + 15
+       print(f"You have {attack} attack")
+       print("You have selected a melee weapon and shield")
+   else:
+       attack = attack + 20
+       print(f"You have {attack} attack")
+       print("You have selected a two-handed weapon")
+
+elif player_class == "M":
+    player_weapon = input("Select your weapon:\n"
+"B-Bow\n"
+"S-Staff\n")
+    if player_weapon == "B":
+        attack = attack + 25
+        print(f"You have {attack} attack")
+        print("You have selected a bow weapon")
+    else:
+        attack = attack + 35
+        print(f"You have {attack} attack")
+        print("You have selected a staff")
+
+elif player_class == "R":
+    player_weapon = input("Select your weapon:\n"
+"D-Dagger\n"
+"H-Hammer\n")
+    if player_weapon == "D":
+        attack = attack + 20
+        print(f"You have {attack} attack")
+    else:
+        attack = attack + 15
+        print(f"You have {attack} attack")
+        print("You have selected a hammer")
 
 
 
